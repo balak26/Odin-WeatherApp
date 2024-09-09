5c480eb5891c1280346c3ed2f2b16da2cef8cb15
balak26 <bamlakgezahegn26@gmail.com>
2024-09-09 20:19:27 +0300

    listed and destructured neccesary key value pairs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import "./style.css";
 
 const key = `QARTB88LAKL4DR7RPNDA87UZF`;
-const place = "turin";
-const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?key=${key}`;
-(async function getWeather() {
+const location = document.querySelector("#city");
+
+async function getWeather(e) {
+  e.preventDefault();
+  const place = location.value;
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?key=${key}`;
   try {
     const res = await fetch(url, { mode: "cors" });
     const weather = await res.json();
-    console.log(weather.currentConditions);
+    const { currentConditions, days, address } = weather;
+    const { windspeed, temp, humidity, conditions, precipprob } =
+      currentConditions;
+    const today = days[0].datetime;
+    const dateObj = new Date(today);
+    const opt = { month: "short", day: "2-digit", year: "numeric" };
+    const formattedDate = dateObj.toLocaleDateString("en-us", opt);
   } catch (err) {
     console.log(err);
   }
-})();
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", getWeather);
